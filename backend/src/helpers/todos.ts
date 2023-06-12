bad305dde7575eea58e81a665eae2c23758ad492
Nguyễn Hoàng Minh <minhnh421999@gmail.com>
2023-06-12 11:22:19 +0700

    add logic create and get todos

--- backend/src/helpers/todos.ts
+++ backend/src/helpers/todos.ts
@@ -8,3 +8,30 @@ import * as uuid from 'uuid'
 import * as createError from 'http-errors'
 
 // TODO: Implement businessLogic
+const todoAccess = new TodosAccess();
+
+export async function createTodo(
+    createTodoRequest: CreateTodoRequest,
+    jwtToken: string
+): Promise<TodoItem> {
+
+    const todoId = uuid.v4()
+    const userId = 'user' //getUserId(jwtToken)
+
+    return await todoAccess.createTodo({
+        userId: userId,
+        todoId: todoId,
+        createdAt: new Date().toISOString(),
+        name: createTodoRequest.name,
+        dueDate: createTodoRequest.name,
+        done: false
+    })
+}
+
+export async function getTodos(
+  jwtToken: string
+): Promise<TodoItem[]> {
+  const userId = 'user' //getUserId(jwtToken)
+
+  return todoAccess.getTodos(userId)
+}
--- backend/src/helpers/todosAcess.ts
+++ backend/src/helpers/todosAcess.ts
@@ -10,3 +10,48 @@ const XAWS = AWSXRay.captureAWS(AWS)
 const logger = createLogger('TodosAccess')
 
 // TODO: Implement the dataLayer logic
+
+export class TodosAccess {
+
+  constructor(
+      private readonly docClient: DocumentClient = createDynamoDBClient(),
+      private readonly todosTable = process.env.TODOS_TABLE) {
+  }
+
+  async createTodo(todo: TodoItem): Promise<TodoItem> {
+      await this.docClient.put({
+          TableName: this.todosTable,
+          Item: todo
+      }).promise()
+
+      return todo
+  }
+
+  async getTodos(userId: string): Promise<TodoItem[]> {
+    logger.info('Getting todos for a user')
+
+    const result = await this.docClient.query({
+        TableName: this.todosTable,
+        KeyConditionExpression: 'userId = :userId',
+        ExpressionAttributeValues: {
+            ':userId': userId
+        },
+        ScanIndexForward: true
+    }).promise()
+
+    const items = result.Items
+    return items as TodoItem[]
+  }
+}
+
+function createDynamoDBClient() {
+  if (process.env.IS_OFFLINE) {
+      logger.info('Creating a local DynamoDB instance')
+      return new AWS.DynamoDB.DocumentClient({
+          region: 'localhost',
+          endpoint: 'http://localhost:8000'
+      })
+  }
+
+  return new AWS.DynamoDB.DocumentClient()
+}
